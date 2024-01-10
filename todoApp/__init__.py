2daddfb0fe42f8d7afb928c833efc237474cdbbb
Rosa T <rosa.tomalin@gmail.com>
2024-01-10 20:02:32 +0000

    inital database setup

--- todoApp/__init__.py
+++ todoApp/__init__.py
@@ -1,12 +1,31 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
+from .extensions.db import db
 
-app = Flask(__name__)
-
-
-@app.get('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+load_dotenv()
 
 
-if __name__ == '__main__':
-    app.run()
+def create_app(test_config=None):
+
+    db_host = os.environ.get("DB_HOST")
+    db_user = os.environ.get("DB_USER")
+    db_password = os.environ.get("DB_PASSWORD")
+
+    app = Flask(__name__, instance_relative_config=True)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{db_password}@{db_host}/flask_todo_db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+
+    @app.get('/')
+    def hello_world():
+        return "Hello World!"
+
+    return app
